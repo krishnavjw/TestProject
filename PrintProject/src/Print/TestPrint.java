593c7d4e92adc756efbc0f989ab9ab97b0bab9fa
venkata krishna pasupulati <vkrishna@publicstorage.com>
2017-02-24 08:27:35 -0800

    Initial CheckIn Code

---
+++ PrintProject/src/Print/TestPrint.java
@@ -0,0 +1,10 @@
+package Print;
+
+public class TestPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("TestPrint");
+	}
+
+}
---
+++ PrintProject/src/Print/VerifyPrint.java
@@ -0,0 +1,10 @@
+package Print;
+
+public class VerifyPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("VerifiedPrint");
+	}
+
+}
