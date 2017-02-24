35eeebe90d334cbf9c71d57e77f117c0b2d6240a
venkata krishna pasupulati <vkrishna@publicstorage.com>
2017-02-24 08:34:37 -0800

    Added New code

--- PrintProject/src/Print/VerifyPrint.java
+++ PrintProject/src/Print/VerifyPrint.java
@@ -5,6 +5,8 @@ public class VerifyPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("VerifiedPrint");
+//Added new Line of code
+System.out.println("Added new line");
 	}
 
 }
