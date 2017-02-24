fc5f6fe393c0daebcfa6542a450e6e512a47d3fa
vkrishna <vkrishna@PS18-PC0AUMXK.ps.com>
2017-02-24 08:50:41 -0800

    added new change

--- PrintProject/src/Print/TestPrint.java
+++ PrintProject/src/Print/TestPrint.java
@@ -5,6 +5,8 @@ public class TestPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("TestPrint");
+//Added new code
+System.out.println("VerifiedPrint");
 	}
 
 }
